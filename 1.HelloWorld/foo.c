235ed1ca408a608a0257a64f3c3b4fb6feaf8012
Jelin <skjsnb@163.com>
2020-11-03 17:12:28 +0800

    学习到函数了

---
+++ 1.HelloWorld/foo.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+
+void foo(const char* str)
+{
+	printf("%s",str);
+}
+
---
+++ 1.HelloWorld/main.c
@@ -0,0 +1,8 @@
+
+extern void foo(const char* str);
+
+int main()
+{
+	foo("HelloWorld!\r\n");	
+	return 0;
+}
